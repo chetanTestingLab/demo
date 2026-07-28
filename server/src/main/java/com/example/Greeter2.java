3fc8be339c62e9f2b8822a259218a6d29e01bd05
chetan <jadhav.chetankumar@gmail.com>
2026-07-29 05:14:01 +0530

    Naksh added & created new file too

--- server/src/main/java/com/example/Greeter2.java
+++ server/src/main/java/com/example/Greeter2.java
@@ -15,13 +15,8 @@ public class Greeter {
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello How are you, %s!", someone);
-    System.out.println("main added");
-    System.out.println("main added2");
-    System.out.println("main added3");
-    
-    System.out.println("main added4");
-    System.out.println("main added5");
-    System.out.println("main added6");
+    System.out.println("ok1");
+ 
     
   }
 }
